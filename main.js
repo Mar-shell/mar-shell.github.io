3c8b7eefe4ab00d72b6f8b85f74d8ac53be67b12
Max <60938398+Mar-shell@users.noreply.github.com>
2020-11-18 19:15:52 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 var canvas = document.querySelector("canvas")
 var c = canvas.getContext("2d")
-var radius = 200
+var radius = 0,
+    sizeFont = 0
 
 function resizeCanvas()
 {
     canvas.width = window.innerWidth
     canvas.height = window.innerHeight
     
-    if (canvas.width < 400)
+    if (window.innerWidth < 400)
     {
-        radius = 170
+        radius = 150
+        sizeFont = 35
+    }
+    else
+    {
+        radius = 200
+        sizeFont = 45
     }
 }
 resizeCanvas()
@@ -18,7 +25,7 @@ addEventListener("resize", resizeCanvas)
 
 function scene()
 {
-    var clock = new Clock(c,canvas.width/2,canvas.height/2,radius)
+    var clock = new Clock(c,canvas.width/2,canvas.height/2,radius, fontSize)
     c.fillStyle = "#333333"
     c.fillRect(0,0,canvas.width,canvas.height)
     clock.create()
