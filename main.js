610ccaa674d5b9f72d554b459e3ac04fc1408593
Max <60938398+Mar-shell@users.noreply.github.com>
2020-11-18 18:47:48 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 var canvas = document.querySelector("canvas")
 var c = canvas.getContext("2d")
+var radius = 200
 
 function resizeCanvas()
 {
     canvas.width = window.innerWidth
     canvas.height = window.innerHeight
+    
+    if (canvas.width > 400)
+    {
+        radius = 170
+    }
 }
 resizeCanvas()
 
@@ -12,7 +18,7 @@ addEventListener("resize", resizeCanvas)
 
 function scene()
 {
-    var clock = new Clock(c,canvas.width/2,canvas.height/2,200)
+    var clock = new Clock(c,canvas.width/2,canvas.height/2,radius)
     c.fillStyle = "#333333"
     c.fillRect(0,0,canvas.width,canvas.height)
     clock.create()
